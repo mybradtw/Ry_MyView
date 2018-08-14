0b2dba0c35c799049c5af027bab1e4eea6ed0a17
Brad Chao <brad@brad.tw>
2018-08-14 10:37:51 +0800

    v2

--- app/src/main/java/brad/tw/ry_myview/MainActivity.java
+++ app/src/main/java/brad/tw/ry_myview/MainActivity.java
@@ -3,12 +3,25 @@ package brad.tw.ry_myview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private MyView myView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myView = findViewById(R.id.myview);
+
+    }
+
+    public void clear(View view) {
+        myView.clear();
+    }
+
+    public void undo(View view) {
+        myView.undo();
     }
 }
--- app/src/main/java/brad/tw/ry_myview/MyView.java
+++ app/src/main/java/brad/tw/ry_myview/MyView.java
@@ -10,11 +10,22 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class MyView extends View {
+    private LinkedList<LinkedList<HashMap<String,Float>>> lines;
+    private Paint paint;
+
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
         setBackgroundColor(Color.GREEN);
+
+        lines = new LinkedList<>();
+        paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(4);
 
 //        setOnClickListener(new OnClickListener() {
 //            @Override
@@ -28,21 +39,70 @@ public class MyView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        for (LinkedList<HashMap<String,Float>> line : lines){
+            for (int i=1; i<line.size(); i++){
+                HashMap<String,Float> p0 = line.get(i-1);
+                HashMap<String,Float> p1 = line.get(i);
+                canvas.drawLine(p0.get("x"),p0.get("y"),
+                        p1.get("x"),p1.get("y"),
+                        paint);
+            }
+        }
+
+    }
+
+    public void clear(){
+        lines.clear();
+        invalidate();
+    }
+
+    public void undo(){
+        if (lines.size()>0){
+            lines.removeLast();
+            invalidate();
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN){
             Log.v("brad", "down");
+            setFirstPoint(event);
         }else if (event.getAction() == MotionEvent.ACTION_UP){
             Log.v("brad", "up");
         }else if (event.getAction() == MotionEvent.ACTION_MOVE){
             Log.v("brad", "move");
+            setMovePoint(event);
         }
 
 
         //super.onTouchEvent(event);
         return true;
     }
+
+    private void setFirstPoint(MotionEvent event){
+        LinkedList<HashMap<String,Float>> line = new LinkedList<>();
+
+        float ex = event.getX(), ey = event.getY();
+        HashMap<String,Float> point = new HashMap<>();
+        point.put("x", ex); point.put("y", ey);
+        line.add(point);
+
+        lines.add(line);
+
+    }
+
+    private void setMovePoint(MotionEvent event){
+        float ex = event.getX(), ey = event.getY();
+        HashMap<String,Float> point = new HashMap<>();
+        point.put("x", ex); point.put("y", ey);
+        lines.getLast().add(point);
+
+        invalidate();
+    }
+
+
+
 
 }
